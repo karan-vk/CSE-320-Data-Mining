d4e0c7b3ebbd32e367210d403608af66eee8c2ef
Karan V <karanvknarayanan@gmail.com>
2021-09-04 12:06:07 +0530

    identifying variables

--- Day 1/q1.r
+++ Day 1/q1.r
@@ -1,4 +1,9 @@
-library(party)
-data(readingSkills)
-library(arules)
-data(Groceries)
+age <- c(45, 89, 23)
+marital_status <- c("Married", "Unmarried", "Divorce")
+quality <- c("Excellent", "Good", "Poor")
+Time_spent <- c(100, 80, 60)
+num_of_people <- c(2, 6, 3)
+type_of_people <- c("Friends", "Alone", "Others")
+
+data <- data.frame(age, marital_status, quality, Time_spent, num_of_people, type_of_people)
+write.table(data, "data.csv", sep = ",", row.names = FALSE, col.names = TRUE)
